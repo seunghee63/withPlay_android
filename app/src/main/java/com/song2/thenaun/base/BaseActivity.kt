32d9b8a5826d81ec97444782704cdd530648e234
song <0603yang@naver.com>
2021-01-18 04:20:53 +0900

    #3 fix : modify databinding inflate location in BaseActivity

--- app/src/main/java/com/song2/thenaun/base/BaseActivity.kt
+++ app/src/main/java/com/song2/thenaun/base/BaseActivity.kt
@@ -12,8 +12,8 @@ abstract class BaseActivity<VB : ViewDataBinding> : AppCompatActivity() {
     lateinit var binding: VB
 
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutResId)
         binding.lifecycleOwner = this
 
