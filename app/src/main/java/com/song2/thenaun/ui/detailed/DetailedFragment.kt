a6c8e4397849f05399cbdb84d0a835c500f1a5a8
song <0603yang@naver.com>
2021-01-23 19:45:08 +0900

    #8 Modify to operate motionlayout in fragment

--- app/src/main/java/com/song2/thenaun/ui/detailed/DetailedFragment.kt
+++ app/src/main/java/com/song2/thenaun/ui/detailed/DetailedFragment.kt
@@ -4,9 +4,10 @@ import com.song2.thenaun.R
 import com.song2.thenaun.base.BaseFragment
 import com.song2.thenaun.databinding.FragmentDetailedBinding
 
-class DetailedFragment : BaseFragment<FragmentDetailedBinding>(){
+class DetailedFragment : BaseFragment<FragmentDetailedBinding>() {
     override val layoutResId: Int
         get() = R.layout.fragment_detailed
 
     override fun initObserver() {}
+
 }
