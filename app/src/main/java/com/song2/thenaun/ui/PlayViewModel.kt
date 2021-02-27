dbbcd761818d69da69dbe3f051581fba77ee035e
song <0603yang@naver.com>
2021-02-28 06:07:06 +0900

    #14 Add PlayerActivity

--- app/src/main/java/com/song2/thenaun/ui/PlayViewModel.kt
+++ app/src/main/java/com/song2/thenaun/ui/PlayViewModel.kt
@@ -1,15 +1,26 @@
 package com.song2.thenaun.ui
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.song2.thenaun.base.BaseViewModel
 
 class PlayViewModel : BaseViewModel() {
 
-    private val _progress = MutableLiveData<Float>().apply { value = 0f }
-    val progress : LiveData<Float> = _progress
+    private val _videoUrl = MutableLiveData<String>().apply { value = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4" }
+    val videoUrl: LiveData<String> = _videoUrl
 
-    fun setProgress(progress : Float){
+    private val _progress = MutableLiveData<Float>().apply { value = 0f }
+    val progress: LiveData<Float> = _progress
+
+    fun setProgress(progress: Float) {
         _progress.value = progress
+    }
+
+    fun onClickFullScreenBtn(){
+
+    }
+
+    fun onClickFullScreenExitBtn(){
     }
 }
--- app/src/main/java/com/song2/thenaun/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/song2/thenaun/ui/player/PlayerActivity.kt
@@ -1,11 +1,64 @@
 package com.song2.thenaun.ui.player
 
+import com.google.android.exoplayer2.util.Util
 import com.song2.thenaun.R
 import com.song2.thenaun.base.BaseActivity
 import com.song2.thenaun.databinding.ActivityPlayerBinding
+import com.song2.thenaun.ui.PlayViewModel
+import com.song2.thenaun.ui.detailed.CustomExoPlayer
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlayerActivity : BaseActivity<ActivityPlayerBinding>() {
     override val layoutResId: Int = R.layout.activity_player
 
+    private val playViewModel: PlayViewModel by viewModel()
+    private val playerView: CustomExoPlayer by lazy { binding.player }
+
+    private fun initCustomExoPlayer() {
+        val testUrl = playViewModel.videoUrl.value
+
+        binding.apply {
+            vm = playViewModel
+        }
+
+        playerView.initPlayer()
+        if (testUrl != null)
+            playerView.playVideo(testUrl)
+    }
+
     override fun initObserver() {}
+
+    override fun onStart() {
+        super.onStart()
+
+        if (Util.SDK_INT > 23) {
+            initCustomExoPlayer()
+            if (playerView.player != null)
+                playerView.onResume()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (Util.SDK_INT <= 23) {
+            initCustomExoPlayer()
+            if (playerView.player != null)
+                playerView.onResume()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (Util.SDK_INT <= 23 && playerView.player != null)
+            playerView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (Util.SDK_INT > 23 && playerView.player != null)
+            playerView.onPause()
+    }
 }
